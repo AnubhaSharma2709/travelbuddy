5fbe6bddd149d5940791bb3b2c46f246f2fd92f4
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-06-04 00:14:15 +0530

    commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../utilis/utilis.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,43 +16,84 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
-      body: ListView(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child:Column(
+      backgroundColor: Styles.bgColor,
+      body: ListView(children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(children: [
+            const Gap(40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Good Morning",
-                        ),
-                        Text("Book Tickets",
-                        ),
-                      ],
+                    Text("Good Morning", style: Styles.headlineStyle3),
+                    Gap(5),
+                    Text(
+                      "Book Tickets",
+                      style: Styles.headlineStyle2,
                     ),
-                    Container(
-                      height: 75,
-                      width: 75,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          fit:BoxFit.cover,
-                          image:AssetImage("lib/images/homePage_image.png",),
+                  ],
+                ),
+                Container(
+                  height: 75,
+                  width: 75,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        "lib/images/homePage_image.png",
                       ),
                     ),
-                    )
-                  ],
+                  ),
                 )
-              ]
-            ) ,
-          )
-     ]
-      ),
+              ],
+            ),
+            const Gap(25),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 12,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white60,
+              ),
+              child: Row(
+                children: [
+                  const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                      color: Colors.black12),
+                  Text(
+                    'Search',
+                    style: Styles.headlineStyle4,
+                  )
+                ],
+              ),
+            ),
+            Gap(40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Upcoming Flights",
+                  style: Styles.headlineStyle2,
+                ),
+                InkWell(
+                  onTap: (){
+                    print("does not work");
+                  },
+                  child: Text(
+                    "ViewAll",
+                    style: Styles.textStyle.copyWith(color: Styles.primaryColor),
+                  ),
+                )
+              ],
+            ),
+          ]),
+        )
+      ]),
     );
   }
 }
--- lib/utilis/utilis.dart
+++ lib/utilis/utilis.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 Color primary = const Color(0xFF687daf);
-class Styles{
-  static Color primaryColor = const Color(0xFF3b3b3b);
-  static Color textColor= const Color(0xFFeeedf2);
+class Styles {
+  static Color primaryColor = primary;
+  static Color textColor = const Color(0xFF3b3b3b);
+  static Color bgColor = const Color(0xFFeeedf2);
   static Color blueColor = const Color(0xFF526799);
+
   static TextStyle textStyle = TextStyle(fontSize: 16,
-  color: textColor,
-  fontWeight: FontWeight.w500,
+    color: textColor,
+    fontWeight: FontWeight.w500,
   );
   static TextStyle headlineStyle = TextStyle(fontSize: 26,
     color: textColor,
     fontWeight: FontWeight.bold,
   );
-  static TextStyle headlineStyle2 = TextStyle(fontSize:21,
+  static TextStyle headlineStyle2 = TextStyle(fontSize: 21,
     color: textColor,
     fontWeight: FontWeight.bold,
   );
-  static TextStyle headlineStyle3 = TextStyle(fontSize:17 ,
+  static TextStyle headlineStyle3 = TextStyle(fontSize: 17,
     color: textColor,
     fontWeight: FontWeight.w500,
   );
-  static TextStyle headlineStyle4 = TextStyle(fontSize:14 ,
+  static TextStyle headlineStyle4 = TextStyle(fontSize: 14,
     color: Colors.grey.shade700,
     fontWeight: FontWeight.w500,
   );
+}
