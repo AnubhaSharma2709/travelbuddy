407f2d2cc2ea59ba03b1a896739be1c7a4a11adc
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-06-03 20:04:40 +0530

    commit

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -10,10 +10,23 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My Tickets"),
-      ),
-      body:Center(child: Text("My Body")),
+      bottomNavigationBar:BottomNavigationBar(
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.redAccent[200],
+        unselectedItemColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.airplane_ticket), label: "Ticket"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ) ,
     );
   }
 }
